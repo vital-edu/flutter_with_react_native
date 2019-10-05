c8b4981f414208006a491ff98eec0c2c0f88d825
Eduardo Vital Alencar Cunha <vital@novatics.com.br>
2019-10-05 13:58:54 -0300

    register factory view in app delegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let nativeViewFactory = NativeViewFactory()
+    registrar(forPlugin: "Runner").register(nativeViewFactory, withId: "NativeView")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
