83e537585f1a3f89e07eb9a1f7db0cce335db142
Eduardo Vital Alencar Cunha <vital@novatics.com.br>
2019-10-05 13:34:16 -0300

    create simple view

---
+++ lib/ar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AR extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AR View'),
+      ),
+      body: Center(
+        child: Text('You did it'),
+      ),
+    );
+  }
+}
